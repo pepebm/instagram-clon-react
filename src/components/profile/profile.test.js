d4059b8fb1a555d04ccd3b08fc8a65cc6c1f9f01
José Manuel <jose@thegurucompany.com>
2018-11-14 19:59:27 -0600

    Buggy tests :(

--- src/components/profile/profile.test.js
+++ src/components/profile/profile.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Profile from './profile';
+import {
+    BrowserRouter as Router
+} from 'react-router-dom';
+import { mount } from 'enzyme';
+import withAuthorization from '../withAuthorization';
+
+
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render( < Profile / > , div);
+    const app = mount(<Router>< Profile / ></Router>);
+    ReactDOM.render(app, div);
     ReactDOM.unmountComponentAtNode(div);
 });
