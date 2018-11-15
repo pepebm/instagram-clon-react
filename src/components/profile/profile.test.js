ab00200f772909274cffb0d488b2dd9ebc86a434
José Manuel <jose@thegurucompany.com>
2018-11-15 12:55:23 -0600

    Add coverage test

--- src/components/profile/profile.test.js
+++ src/components/profile/profile.test.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Profile from './profile';
 import {
     BrowserRouter as Router
 } from 'react-router-dom';
-import { mount } from 'enzyme';
 import withAuthorization from '../withAuthorization';
 
 
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    const app = mount(<Router>< Profile / ></Router>);
-    ReactDOM.render(app, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const app = global.shallow(<Router>< Profile / ></Router>);
 });
